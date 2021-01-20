37b9e24ef440262082c974ede71b257c226f611e
Julienne San Luis <julienne.codes@gmail.com>
2021-01-19 21:30:47 -0500

    shortened code for work, break, and big break sessions in test

--- src/App.test.js
+++ src/App.test.js
@@ -12,82 +12,58 @@ const workTimerMs = workTimer * 1000;
 const breakTimerMs = breakTimer * 1000;
 const bigBreakTimerMs = bigBreakTimer * 1000;
 
-const breakTimerRegEx = new RegExp(stringTimer(breakTimer));
-
-
 test('timer counts down', ()=>{
     jest.useFakeTimers();
-
     render(<App />);
     const startButton = screen.getByRole('button', {name: 'Start'});
-    userEvent.click(startButton);
     
-    act(()=>{jest.advanceTimersByTime(workTimerMs)});
-    // jest.advanceTimersByTime(workTimerMs);
-
-    expect(screen.getByText(breakTimerRegEx)).toBeInTheDocument();
-    // screen.debug();
+    workSession(startButton);
+    expect(screen.getByText(/break/i)).toBeInTheDocument();
 });
 
 test('timer does not go past zero', () => {
     jest.useFakeTimers();
     render(<App />);
     const startButton = screen.getByRole('button', {name: 'Start'});
-    userEvent.click(startButton);
-
-    act(()=>{jest.advanceTimersByTime(workTimerMs + 1000)});
-
-    expect(screen.getByText(breakTimerRegEx)).toBeInTheDocument();
-    // screen.debug();
+    
+    workSession(startButton);
+    expect(screen.getByText(/break/i)).toBeInTheDocument();
 });
 
-test('timer switches session when timer runs out', () => {
+test('when work session ends, session is changed to break', ()=>{
     jest.useFakeTimers();
     render(<App />);
     const startButton = screen.getByRole('button', {name: 'Start'});
-    userEvent.click(startButton);
-    act(()=>{jest.advanceTimersByTime(workTimerMs)});
 
+    workSession(startButton);
     expect(screen.getByText(/break/i)).toBeInTheDocument();
-    // screen.debug();
-
-    userEvent.click(startButton);
-    act(()=>{jest.advanceTimersByTime(breakTimerMs)});
-    expect(screen.getByText(/work/i)).toBeInTheDocument();
-    // screen.debug();
 });
 
+test('when break session ends, session is changed to work', ()=>{
+    jest.useFakeTimers();
+    render(<App />);
+    const startButton = screen.getByRole('button', {name: 'Start'});
+
+    workSession(startButton);
+    breakSession(startButton);
+    expect(screen.getByText(/work/i)).toBeInTheDocument();
+});
 
 test('when four work sessions are completed, session is changed to big break', ()=>{
     jest.useFakeTimers();
     render(<App />);
     const startButton = screen.getByRole('button', {name: 'Start'});
     
-    // workBreakInterval(startButton);
-    expect(screen.getByText(/work/i)).toBeInTheDocument();
-    userEvent.click(startButton);
-    act(()=>{jest.advanceTimersByTime(workTimerMs)});
-    expect(screen.getByText(/break/i)).toBeInTheDocument();
-    userEvent.click(startButton);
-    act(()=>{jest.advanceTimersByTime(breakTimerMs)});
+    workSession(startButton);
+    breakSession(startButton);
 
-    expect(screen.getByText(/work/i)).toBeInTheDocument();
-    userEvent.click(startButton);
-    act(()=>{jest.advanceTimersByTime(workTimerMs)});
-    expect(screen.getByText(/break/i)).toBeInTheDocument();
-    userEvent.click(startButton);
-    act(()=>{jest.advanceTimersByTime(breakTimerMs)});
+    workSession(startButton);
+    breakSession(startButton);
 
-    expect(screen.getByText(/work/i)).toBeInTheDocument();
-    userEvent.click(startButton);
-    act(()=>{jest.advanceTimersByTime(workTimerMs)});
-    expect(screen.getByText(/break/i)).toBeInTheDocument();
-    userEvent.click(startButton);
-    act(()=>{jest.advanceTimersByTime(breakTimerMs)});
+    workSession(startButton);
+    breakSession(startButton);
 
-    expect(screen.getByText(/work/i)).toBeInTheDocument();
-    userEvent.click(startButton);
-    act(()=>{jest.advanceTimersByTime(workTimerMs)});
+    workSession(startButton);
 
     expect(screen.getByText(/big break/i)).toBeInTheDocument();
 });
@@ -97,48 +73,36 @@ test('when big break timer runs out, work timer is displayed', ()=>{
     render(<App />);
     const startButton = screen.getByRole('button', {name: 'Start'});
     
-    // workBreakInterval(startButton);
+    workSession(startButton);
+    breakSession(startButton);
+
+    workSession(startButton);
+    breakSession(startButton);
+
+    workSession(startButton);
+    breakSession(startButton);
+
+    workSession(startButton);
+    bigBreakSession(startButton);
+
+    expect(screen.getByText(/work/i)).toBeInTheDocument();
+});
+//------------------------------------------------------------------------------
+function workSession(startButton){
     expect(screen.getByText(/work/i)).toBeInTheDocument();
     userEvent.click(startButton);
     act(()=>{jest.advanceTimersByTime(workTimerMs)});
+};
+
+function breakSession(startButton){
     expect(screen.getByText(/break/i)).toBeInTheDocument();
     userEvent.click(startButton);
     act(()=>{jest.advanceTimersByTime(breakTimerMs)});
+}
 
-    expect(screen.getByText(/work/i)).toBeInTheDocument();
-    userEvent.click(startButton);
-    act(()=>{jest.advanceTimersByTime(workTimerMs)});
-    expect(screen.getByText(/break/i)).toBeInTheDocument();
-    userEvent.click(startButton);
-    act(()=>{jest.advanceTimersByTime(breakTimerMs)});
-
-    expect(screen.getByText(/work/i)).toBeInTheDocument();
-    userEvent.click(startButton);
-    act(()=>{jest.advanceTimersByTime(workTimerMs)});
-    expect(screen.getByText(/break/i)).toBeInTheDocument();
-    userEvent.click(startButton);
-    act(()=>{jest.advanceTimersByTime(breakTimerMs)});
-
-    expect(screen.getByText(/work/i)).toBeInTheDocument();
-    userEvent.click(startButton);
-    act(()=>{jest.advanceTimersByTime(workTimerMs)});
-
+function bigBreakSession(startButton){
     expect(screen.getByText(/big break/i)).toBeInTheDocument();
     userEvent.click(startButton);
     act(()=>{jest.advanceTimersByTime(bigBreakTimerMs)});
-
-    expect(screen.getByText(/work/i)).toBeInTheDocument();
-
-
-    
-});
-//------------------------------------------------------------------------------
-function stringTimer(timeRemaining){
-    const minutesRemaining = Math.floor(timeRemaining / 60);
-    const secondsRemaining = timeRemaining % 60;
-
-    const minutes = `${minutesRemaining}`.padStart(2, "0");
-    const seconds = `${secondsRemaining}`.padStart(2, "0");
-
-    return `${minutes}:${seconds}`;
 }
+
