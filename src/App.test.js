7b0a2e107a00d08ebb08a7fe8be1d6b2744119b8
Julienne San Luis <julienne.codes@gmail.com>
2021-01-19 23:20:18 -0500

    added tests for timer controls

--- src/App.test.js
+++ src/App.test.js
@@ -12,6 +12,9 @@ const workTimerMs = workTimer * 1000;
 const breakTimerMs = breakTimer * 1000;
 const bigBreakTimerMs = bigBreakTimer * 1000;
 
+const regexWorkTimer = new RegExp(stringTimer(workTimer));
+
+
 beforeEach(()=>{
     jest.useFakeTimers();
     render(<App />);
@@ -64,6 +67,55 @@ test('when big break timer runs out, work timer is displayed', ()=>{
 
     expect(screen.getByText(/work/i)).toBeInTheDocument();
 });
+
+test('start button is replaced with stop button when clicked during work sessions', () => {
+    const startButton = screen.getByRole('button', {name: 'Start'});
+
+    expect(screen.getByText(/Start/)).toBeInTheDocument();
+    userEvent.click(startButton);
+    expect(screen.queryByText(/Start/)).not.toBeInTheDocument();
+    expect(screen.getByText(/Stop/)).toBeInTheDocument();
+
+});
+
+test('start button is replaced with skip button during break sessions', ()=>{
+    const startButton = screen.getByRole('button', {name: 'Start'});
+
+    expect(screen.getByText(/Start/)).toBeInTheDocument();
+    userEvent.click(startButton);
+    expect(screen.queryByText(/Start/)).not.toBeInTheDocument();
+    expect(screen.getByText(/Skip/)).toBeInTheDocument();
+});
+
+test('stop button resets the timer when the work session is interrupted', ()=>{
+    const startButton = screen.getByRole('button', {name: 'Start'});
+
+    expect(screen.getByText(/work/i)).toBeInTheDocument();
+    userEvent.click(startButton);
+    act(()=> jest.advanceTimersByTime(1000));
+    expect(screen.getByText(/Stop/)).toBeInTheDocument();
+
+    const stopButton = screen.getByRole('button', {name: 'Stop'});
+    userEvent.click(stopButton);
+
+    expect(screen.getByText(regexWorkTimer)).toBeInTheDocument();
+
+});
+
+test('skip button skips the break session', ()=>{
+    const startButton = screen.getByRole('button', {name: 'Start'});
+
+    workSession(startButton);
+    expect(screen.getByText(/break/)).toBeInTheDocument();
+    userEvent.click(startButton);
+    act(()=> jest.advanceTimersByTime(1000));
+    expect(screen.getByText(/Skip/)).toBeInTheDocument();
+
+    const skipButton = screen.getByRole('button', {name: 'Skip'});
+    userEvent.click(skipButton);
+
+    expect(screen.getByText(/work/)).toBeInTheDocument();
+});
 //------------------------------------------------------------------------------
 function doXTimes(callback, x){
     for (let i = 0; i < x; i++) {
@@ -94,3 +146,12 @@ function bigBreakSession(startButton){
     act(()=>{jest.advanceTimersByTime(bigBreakTimerMs)});
 }
 
+function stringTimer(timeRemaining){
+    const minutesRemaining = Math.floor(timeRemaining / 60);
+    const secondsRemaining = timeRemaining % 60;
+
+    const minutes = `${minutesRemaining}`.padStart(2, "0");
+    const seconds = `${secondsRemaining}`.padStart(2, "0");
+
+    return `${minutes}:${seconds}`;
+}
