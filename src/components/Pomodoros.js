b236065f5724bb5ad9fbc1646ce3245740730bed
Julienne San Luis <julienne.codes@gmail.com>
2021-01-23 22:58:32 -0500

    added indicator for pomodoros to be completed

--- src/components/Pomodoros.js
+++ src/components/Pomodoros.js
@@ -1,20 +1,35 @@
 import {Pomodoro} from './Pomodoro';
+import { PomodoroSlot } from './PomodoroSlot';
 
 export function Pomodoros({session, completedPomodoros}){
     if(session === 'big break'){
         const finishedPomodoros = 4;
         return (
-                <ul className="pomodoros">
+            <div className="pomodoros">
+                <ul className="pomodoro-slots">
+                    {Array(4).fill(null).map((x, i)=> <PomodoroSlot key = {i}/> )}
+                </ul>
+
+                <ul className="filled-pomodoros">
                     {Array(finishedPomodoros).fill(null).map((x, i) => <Pomodoro key = {i}/>)}
                 </ul>
+
+            </div>
+               
         )
 
     } else {
         const finishedPomodoros = completedPomodoros % 4; 
         return (
-                <ul className="pomodoros">
+            <div className="pomodoros">
+                <ul className="pomodoro-slots">
+                    {Array(4).fill(null).map((x, i)=> <PomodoroSlot key = {i}/> )}
+                </ul>
+                <ul className="filled-pomodoros">
                     {Array(finishedPomodoros).fill(null).map((x, i) => <Pomodoro key = {i}/>)}
                 </ul>
+            </div>
+                
         )
     }
 }
--- src/components/TimerControls.js
+++ src/components/TimerControls.js
@@ -4,7 +4,9 @@ export function TimerControls({active, session, startTimer, stopTimer, skipTimer
     } 
     else if(active && session === 'break'){
         return(<button className="timer-control" onClick={skipTimer}>Skip</button>)
-
+    }
+    else if(active && session === 'big break'){
+        return(<button className="timer-control" onClick={skipTimer}>Skip</button>)
     } else {
         return(<button className="timer-control" onClick={startTimer}>Start</button>)
     }
