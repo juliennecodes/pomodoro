c5fb4448e5e0eef7c8222b0289baf788a617bcd1
Julienne San Luis <julienne.codes@gmail.com>
2021-01-26 17:02:39 -0500

    lengthy solution to filled and unfilled pomodoro svgs

--- src/components/Pomodoro.js
+++ src/components/Pomodoro.js
@@ -1,17 +1,33 @@
-export function Pomodoro(){
-    return(
-        <li className="pomodoro">
-            <svg 
-                xmlns="http://www.w3.org/2000/svg"
-                fill="hsl(354, 59%, 57%)"
-                width="24" 
-                height="24" 
-                viewBox="0 0 24 24">
-                <circle cx="12" cy="12" r="12"/>
-            </svg>
-        </li>
-        
-    )
+export function Pomodoro({filled}){
+    if(filled) {
+        return(
+            <li className="pomodoro">
+                <svg 
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="hsl(354, 59%, 57%)"
+                    width="24" 
+                    height="24" 
+                    viewBox="0 0 24 24">
+                    <circle cx="12" cy="12" r="12"/>
+                </svg>
+            </li>
+        )
+    } 
+    
+    else {
+        return(
+            <li className="pomodoro">
+                <svg xmlns="http://www.w3.org/2000/svg"
+                    fill="hsl(354, 59%, 57%)"
+                    width="24" 
+                    height="24" 
+                    viewBox="0 0 24 24">
+                    <path d="M12 1c6.065 0 11 4.935 11 11s-4.935 11-11 11-11-4.935-11-11 4.935-11 11-11zm0-1c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12z"/>
+                </svg>
+            </li>
+            
+        )
+    }   
 }
 
 // export function Pomodoro(){
--- src/components/Pomodoros.js
+++ src/components/Pomodoros.js
@@ -1,35 +1,59 @@
 import {Pomodoro} from './Pomodoro';
-import { PomodoroSlot } from './PomodoroSlot';
 
 export function Pomodoros({session, completedPomodoros}){
+    const finishedPomodoros = completedPomodoros % 4;
+
     if(session === 'big break'){
-        const finishedPomodoros = 4;
         return (
             <div className="pomodoros">
-                <ul className="pomodoro-slots">
-                    {Array(4).fill(null).map((x, i)=> <PomodoroSlot key = {i}/> )}
+                <ul className="pomodorosUl">
+                    {Array(4).fill(null).map(()=> <Pomodoro filled={true}/>)}
                 </ul>
-
-                <ul className="filled-pomodoros">
-                    {Array(finishedPomodoros).fill(null).map((x, i) => <Pomodoro key = {i}/>)}
-                </ul>
-
             </div>
-               
         )
-
-    } else {
-        const finishedPomodoros = completedPomodoros % 4; 
+    }
+    
+    if(finishedPomodoros === 0) {
         return (
             <div className="pomodoros">
-                <ul className="pomodoro-slots">
-                    {Array(4).fill(null).map((x, i)=> <PomodoroSlot key = {i}/> )}
-                </ul>
-                <ul className="filled-pomodoros">
-                    {Array(finishedPomodoros).fill(null).map((x, i) => <Pomodoro key = {i}/>)}
+                <ul className="pomodorosUl">
+                    {Array(4).fill(null).map(()=> <Pomodoro filled={false}/>)}
                 </ul>
             </div>
-                
+        )
+    }
+
+    else if (finishedPomodoros === 1){
+        return (
+            <div className="pomodoros">
+                <ul className="pomodorosUl">
+                    {Array(1).fill(null).map(()=> <Pomodoro filled={true}/>)}
+                    {Array(3).fill(null).map(()=> <Pomodoro filled={false}/>)}
+                </ul>
+            </div>
+        )
+    }
+
+    else if (finishedPomodoros === 2) {
+        return (
+            <div className="pomodoros">
+                <ul className="pomodorosUl">
+                    {Array(2).fill(null).map(()=> <Pomodoro filled={true}/>)}
+                    {Array(2).fill(null).map(()=> <Pomodoro filled={false}/>)}
+                </ul>
+            </div>
+        )
+    }
+    
+    else if (finishedPomodoros === 3) {
+        return (
+            <div className="pomodoros">
+                <ul className="pomodorosUl">
+                    {Array(3).fill(null).map(()=> <Pomodoro filled={true}/>)}
+                    {Array(1).fill(null).map(()=> <Pomodoro filled={false}/>)}
+                </ul>
+            </div>
         )
     }
 }
+
