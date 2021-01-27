f0b14eff03e3bc3f9a6a5af782c2ef5519f1d081
Julienne San Luis <julienne.codes@gmail.com>
2021-01-26 20:20:21 -0500

    replaced lengthy code with a for loop

--- src/components/Pomodoros.js
+++ src/components/Pomodoros.js
@@ -1,59 +1,28 @@
 import {Pomodoro} from './Pomodoro';
 
 export function Pomodoros({session, completedPomodoros}){
-    const finishedPomodoros = completedPomodoros % 4;
-
     if(session === 'big break'){
         return (
             <div className="pomodoros">
                 <ul className="pomodorosUl">
-                    {Array(4).fill(null).map(()=> <Pomodoro filled={true}/>)}
-                </ul>
-            </div>
-        )
-    }
-    
-    if(finishedPomodoros === 0) {
-        return (
-            <div className="pomodoros">
-                <ul className="pomodorosUl">
-                    {Array(4).fill(null).map(()=> <Pomodoro filled={false}/>)}
+                    {Array(4).fill(null).map((currentValue, n)=> <Pomodoro filled={true} key={n}/>)}
                 </ul>
             </div>
         )
     }
 
-    else if (finishedPomodoros === 1){
-        return (
-            <div className="pomodoros">
-                <ul className="pomodorosUl">
-                    {Array(1).fill(null).map(()=> <Pomodoro filled={true}/>)}
-                    {Array(3).fill(null).map(()=> <Pomodoro filled={false}/>)}
-                </ul>
-            </div>
-        )
-    }
-
-    else if (finishedPomodoros === 2) {
-        return (
-            <div className="pomodoros">
-                <ul className="pomodorosUl">
-                    {Array(2).fill(null).map(()=> <Pomodoro filled={true}/>)}
-                    {Array(2).fill(null).map(()=> <Pomodoro filled={false}/>)}
-                </ul>
-            </div>
-        )
-    }
-    
-    else if (finishedPomodoros === 3) {
-        return (
-            <div className="pomodoros">
-                <ul className="pomodorosUl">
-                    {Array(3).fill(null).map(()=> <Pomodoro filled={true}/>)}
-                    {Array(1).fill(null).map(()=> <Pomodoro filled={false}/>)}
-                </ul>
-            </div>
-        )
+    const finishedPomodoros = completedPomodoros % 4;
+    for(let i = 0; i < 4; i++){
+        if(i === finishedPomodoros){
+            return (
+                <div className="pomodoros">
+                    <ul className="pomodorosUl">
+                        {Array(i).fill(null).map((currentValue, n)=> <Pomodoro filled={true} key={n}/>)}
+                        {Array(4 - i).fill(null).map((currentValue, n)=> <Pomodoro filled={false} key={n}/>)}
+                    </ul>
+                </div>
+            )
+        }
     }
 }
 
